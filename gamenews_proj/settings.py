6955f260503e75b3edac0a657de3575a620abf91
xRA1Nx <stwski@gmail.com>
2022-08-06 12:30:44 +0300

    add secret key to env

--- gamenews_proj/settings.py
+++ gamenews_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8toa548#o(^!io2vv5wdcdu)%6yr&qjjpwuz^*9dsokzd$$o6n'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
